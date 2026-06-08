3cabf44180fa13826e832452973bea576f658a85
Linus Johansson <linus.123@hotmail.com>
2026-06-08 11:06:32 +0200

    Gjør det likt som for spleis-avstemming

--- src/main/resources/db/migration/V4__grant_purring_user.sql
+++ src/main/resources/db/migration/V4__grant_purring_user.sql
@@ -1,3 +1,9 @@
-GRANT SELECT, INSERT, UPDATE ON dialog TO purring;
-GRANT SELECT, INSERT ON outbox TO purring;
-GRANT USAGE ON ALL SEQUENCES IN SCHEMA public TO purring;
+DO $$
+BEGIN
+    IF EXISTS (SELECT FROM pg_roles WHERE rolname = 'purring') THEN
+        GRANT SELECT, INSERT, UPDATE ON dialog TO purring;
+        GRANT SELECT, INSERT ON outbox TO purring;
+        GRANT USAGE ON ALL SEQUENCES IN SCHEMA public TO purring;
+    END IF;
+END
+$$;
